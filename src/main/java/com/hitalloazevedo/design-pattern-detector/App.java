50bdd738c982ba377f5bc409aa783f4727534e98
Azevedo, Hitallo <azevedohitallo@gmail.com>
2026-07-14 14:14:52 -0300

    chore: init maven project

---
+++ src/main/java/com/hitalloazevedo/design-pattern-detector/App.java
@@ -0,0 +1,10 @@
+package com.hitalloazevedo.design-pattern-detector;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

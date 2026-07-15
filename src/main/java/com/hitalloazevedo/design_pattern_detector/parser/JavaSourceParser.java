f6be9af91d560e3fd9374788d638e6d1a6919621
Azevedo, Hitallo <azevedohitallo@gmail.com>
2026-07-14 22:22:59 -0300

    refactor: refactor java source parser to use configuration

--- src/main/java/com/hitalloazevedo/design_pattern_detector/parser/JavaSourceParser.java
+++ src/main/java/com/hitalloazevedo/design_pattern_detector/parser/JavaSourceParser.java
@@ -1,36 +1,71 @@
 package com.hitalloazevedo.design_pattern_detector.parser;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ParseResult;
-import com.github.javaparser.ast.CompilationUnit;
-
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class JavaSourceParser {
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ParserConfiguration;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.symbolsolver.JavaSymbolSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.JavaParserTypeSolver;
+import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeSolver;
+
+public final class JavaSourceParser {
+
     private final JavaParser parser;
 
-    public JavaSourceParser() {
-        this.parser = new JavaParser();
+    public JavaSourceParser(Iterable<Path> sourceRoots) {
+        CombinedTypeSolver typeSolver = new CombinedTypeSolver();
+
+        typeSolver.add(new ReflectionTypeSolver());
+
+        for (Path sourceRoot : sourceRoots) {
+            typeSolver.add(
+                    new JavaParserTypeSolver(sourceRoot));
+        }
+
+        JavaSymbolSolver symbolSolver = new JavaSymbolSolver(typeSolver);
+
+        ParserConfiguration configuration = new ParserConfiguration()
+                .setLanguageLevel(
+                        ParserConfiguration.LanguageLevel.JAVA_22)
+                .setSymbolResolver(symbolSolver);
+
+        this.parser = new JavaParser(configuration);
     }
 
-    public CompilationUnit parse(Path sourceFile) throws IOException {
-        ParseResult<CompilationUnit> result = parser.parse(sourceFile);
+    public CompilationUnit parse(Path sourceFile)
+            throws IOException {
 
-        if (result.isSuccessful() && result.getResult().isPresent()) {
+        Objects.requireNonNull(
+                sourceFile,
+                "sourceFile cannot be null");
+
+        Path normalizedSourceFile = sourceFile
+                .toAbsolutePath()
+                .normalize();
+
+        ParseResult<CompilationUnit> result = parser.parse(normalizedSourceFile);
+
+        if (result.isSuccessful()
+                && result.getResult().isPresent()) {
             return result.getResult().get();
         }
 
         String problems = result.getProblems()
                 .stream()
                 .map(Object::toString)
-                .reduce("", (current, problem) ->
-                        current + System.lineSeparator() + problem);
+                .collect(Collectors.joining(
+                        System.lineSeparator()));
 
         throw new IllegalArgumentException(
                 "Não foi possível analisar o arquivo: "
-                        + sourceFile
-                        + problems
-        );
+                        + normalizedSourceFile
+                        + System.lineSeparator()
+                        + problems);
     }
 }
